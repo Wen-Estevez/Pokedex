cadf3aab3f0c8a289927ed360b78aebf3c17ee0c
Hernán David Figueroa <davshn@gmail.com>
2021-09-30 22:01:16 -0500

    Promise

--- dex.js
+++ dex.js
@@ -1,36 +1,28 @@
-const baseURL ="https://pokeapi.co/api/v2/pokemon/";
-const pokemon = document.getElementById("pokemonName");
-const buttonSearch = document.getElementById("searchPokemon");
-const buttonCancel = document.getElementById("cancelPokemon");
-const appNode = document.getElementById("dex")
+const baseUrl = `https://pokeapi.co/api/v2/pokemon/`;
+const pokemon = document.getElementById('pokemonName');
+const buttonPokemon = document.getElementById('searchPokemon');
+const buttonClear = document.getElementById('clearPokemon');
+const appNode = document.getElementById('dex');
 
-buttonSearch.addEventListener("click",insertPokemon());
-buttonSearch.addEventListener("touchStart",insertPokemon()); //tocar pantalla movil
-buttonCancel.addEventListener("click",removePokemon());
-buttonCancel.addEventListener("touchStart",removePokemon()); //tocar pantalla movil
+buttonPokemon.addEventListener('click' , insertPokemon);
+buttonPokemon.addEventListener('touchstart' , insertPokemon); //*Tocar la pantalla movil
 
-function insertPokemon(){
-    window.fetch(`${baseURL}${pokemon.value.toLowerCase()}`); //llamada a la api, envia pagina web y valor del pokemon 
-        .then(function (response) { //envia la respuesta de la api como parametro
-            if (response.status===404){
-            alert("Pokemon no encontrado");
-            }
-            else {
-                return response.json();
-            }
-        } )
-        .then(function (responseJSON){
-            const allItems=[]; //guarda lo que se va a mostrar
-            const result=[];  // guarda la info de los pokemon
-            for(let pokemonInfo in responseJSON ){
-                result.push(pokemonInfo,responseJSON[pokemoInfo]) //Agrega al arreglo result el resultado de la consulta
-            }
-        }
+buttonClear.addEventListener('click' , deletePokemons);
+buttonClear.addEventListener('touchstart' , deletePokemons); //* Tocar la pantalla movil
 
+async function insertPokemon() {
+  try {
+    const res = await fetch(`${baseUrl}${pokemon.value.toLocaleLowerCase()}`)
+    const pokemonDataJSON = await res.json()
 
+    const allItems = [];
+    const result = []; //*Guardaremos la respuesta en el array
 
-        )
-}
+    for (let pokemonInfo in pokemonDataJSON) { //*Convertimos el objeto JSON a array
+      result.push([pokemonInfo , pokemonDataJSON[pokemonInfo]]);
+    }
+      
+      
 function removePokemon(){
 
 
